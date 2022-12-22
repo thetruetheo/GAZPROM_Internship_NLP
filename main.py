e5fada0bec5a2effcb829f0d3630febba3f7f7f9
thetruetheo <thetruetheo2108@gmail.com>
2022-12-22 09:03:05 +0300

    Произведён рефакторинг и упрощение кода. Добавлена универсальная обработка для .txt файлов.

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
 import os
 from docx import Document
 
-
-
-
-def docx_to_dict(name_of_file):
-    docx_dict={}
-    document=Document(name_of_file)
-    indx = 0
-    for para in document.paragraphs:
-        indx += 1
-        if (len(para.text) > 0):
-            #print("\n paragraph", indx, "is")
-            #print(para.text)
-            docx_dict[indx] = para.text
-    return docx_dict
+def text_extractor(path):
+    doc_obj=open(path,"rb")
+    doc_reader=Document(doc_obj)
+    data=""
+    for p in doc_reader.paragraphs:
+        data +=p.text+"\n"
+    return data
 
 directory="test_documents"
-docx_content={}
+
+document_text=""
 
 for filename in os.listdir(directory):
     if filename.endswith(".docx") or filename.endswith(".doc"):
-        #print('file name is',filename)
+        print("\nНачалась обработка документа: \n")
         print(os.path.join(directory, filename))
         path_to_docx = os.path.join(directory, filename)
-        docx_content[path_to_docx] = docx_to_dict(path_to_docx)
+        document_text=text_extractor(path_to_docx)
+        #print(document_text)
 
-        writeable_content = list(docx_content.items())
         with open(str(path_to_docx)+'.txt', 'w', encoding="utf-8") as writer:
-            for value in writeable_content:
-                writer.write(str(value))
-        writeable_content = []
+            for value in document_text:
 
-        docx_content = {}
-        print("\nОбработка документа окончена\n")
+                writer.write(value)
+        document_text=""
+        print("\nОбработка документа завершена.\n")
 
 
 
